65157c97042181d4cc2f3242d53b22d93c34057a
Omanshu Mahawar <omanshumahawar.181co237@nitk.edu.in>
2023-09-02 17:29:14 +0530

    Fix routing bug

---
+++ build/precache-manifest.a2057fc96cc9dba73fdb83ab79882647.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf3a097bfd1647d110397259be99aa21",
+    "url": "/Socio-Engage/index.html"
+  },
+  {
+    "revision": "98c08769adeb6299f33d",
+    "url": "/Socio-Engage/static/css/2.3cf5d70f.chunk.css"
+  },
+  {
+    "revision": "f9388ef78c51fed59335",
+    "url": "/Socio-Engage/static/css/main.fc8c3ef8.chunk.css"
+  },
+  {
+    "revision": "98c08769adeb6299f33d",
+    "url": "/Socio-Engage/static/js/2.bcae3282.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/Socio-Engage/static/js/2.bcae3282.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f9388ef78c51fed59335",
+    "url": "/Socio-Engage/static/js/main.1a39dbf5.chunk.js"
+  },
+  {
+    "revision": "31376b626681d9528e2c",
+    "url": "/Socio-Engage/static/js/runtime-main.4985923c.js"
+  }
+]);
